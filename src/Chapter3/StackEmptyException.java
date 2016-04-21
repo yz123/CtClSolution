5a74620eba14ba567a6868805aaff92856e4e2ba
Jinhai ZHOU <zhoujinhaixd@gmail.com>
2016-04-21 16:50:19 +0200

    add fixed Division method

---
+++ src/Chapter3/StackEmptyException.java
@@ -0,0 +1,10 @@
+package Chapter3;
+
+public class StackEmptyException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
--- src/Chapter3/ThreeInOne.java
+++ src/Chapter3/ThreeInOne.java
@@ -1,96 +1,72 @@
 package Chapter3;
 
 public class ThreeInOne {
-	// the container of stacks
+	// this question aims to create three stacks out of one array
+	// however, our solution can be generalized to multiple stacks
+	private int stackCapacity;
+	private int numOfStacks = 3;
+	private int size[];
 	private int array[];
-	private static int low1;
-	private static int low2;
-	private static int low3;
-	private static int up1;
-	private static int up2;
-	private static int up3;
-	private static int index1;
-	private static int index2;
-	private static int index3;
-	private static boolean full1;
-	private static boolean full2;
-	private static boolean full3;
-	// constructor of the class
-	public ThreeInOne(int array[]) {
-		if (array.length < 3 ) {
-			System.out.println("Exception: array length not enough");
-		}
-		this.array = array;
-		
-		ThreeInOne.low1 = 0;
-		ThreeInOne.up1 = array.length/3-1;
-		ThreeInOne.index1 = ThreeInOne.low1-1;
-		ThreeInOne.full1 = false;
-		
-		ThreeInOne.low2 = array.length/3;
-		ThreeInOne.up2 = array.length*2/3-1;
-		ThreeInOne.index2 = ThreeInOne.low2-1;
-		ThreeInOne.full2 = false;
-		
-		ThreeInOne.low3 = array.length*2/3;
-		ThreeInOne.up3 = array.length-1;
-		ThreeInOne.index3 = ThreeInOne.low3-1;
-		ThreeInOne.full3 = false;
+
+	public ThreeInOne(int stackCapacity) {
+		this.stackCapacity = stackCapacity;
+		this.array = new int[numOfStacks*stackCapacity];
+		this.size = new int[numOfStacks];
 		
 	} 
 	// Stack 1
-	public void push1(int i) throws Exception {
-		if (index1 < ThreeInOne.up1) {
-			index1 ++;
-			this.array[index1] = i;
-		} else if (index1 == ThreeInOne.up1) {
-			index1 ++;
-			this.array[index1] = i;
-			ThreeInOne.full1 = true;
-		} else {
-			throw new Exception();
+	public void push(int stackNum, int element) throws FullStackException {
+		if (isFull(stackNum)) {
+			throw new FullStackException();
 		}
+		size[stackNum]++;
+		array[topOfStack(stackNum)] = element;		
 	}
-	public boolean isEmpty1(){
-		if (index1<low1) {
+	
+	public int pop(int stackNum) throws StackEmptyException{
+		if (isEmpty(stackNum)) {
+			throw new StackEmptyException();
+		}
+		int element = array[topOfStack(stackNum)];
+		size[stackNum]--;
+		return element;
+	}
+	
+	public int peek(int stackNum)throws StackEmptyException{
+		if (isEmpty(stackNum)) {
+			throw new StackEmptyException();
+		}
+		int element = array[topOfStack(stackNum)];
+		return element;
+	}
+	
+	public boolean isFull(int stackNum) {
+		if (size[stackNum] == this.stackCapacity) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 	
-	public int pop1() throws Exception{
-		if (isEmpty1()) {
-			throw new Exception();
+	public boolean isEmpty(int stackNum){
+		if (size[stackNum] == 0) {
+			return true;
 		} else {
-			int i = this.array[index1];
-			index1 --;
-			return i; 
+			return false;
 		}
 	}
 	
-	public int peek1() throws Exception {
-		if (isEmpty1()) {
-			throw new Exception();
-		} else {
-			int i = this.array[index1];
-			return i; 
-		}
+	public int topOfStack(int stackNum) {
+		int offset = stackNum*stackCapacity;
+		return offset+size[stackNum]-1;
 	}
-	
-	// Stack2
-	
-	// Stack3
 
 	public static void main(String args[]) throws Exception {
-		System.out.println("hello world");
-		int array[] = new int[120];
-		ThreeInOne three = new ThreeInOne(array);
-		three.push1(4);
-		three.push1(5);
-		System.out.println(three.pop1());
-		System.out.println(three.pop1());
-		System.out.println(three.isEmpty1());
+		ThreeInOne stack = new ThreeInOne(60);
+		stack.push(0, 5);
+		stack.push(0, 6);
+		System.out.println(stack.pop(0));
+		System.out.println(stack.pop(0));
 	}
 
 }
