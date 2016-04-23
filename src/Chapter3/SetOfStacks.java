e76354b2cc442891256eebbe56a2d1cc57b115d8
Jinhai ZHOU <zhoujinhaixd@gmail.com>
2016-04-23 17:14:36 +0200

    a concise version to SetOfStacks

--- src/Chapter3/SetOfStacks.java
+++ src/Chapter3/SetOfStacks.java
@@ -2,101 +2,51 @@ package Chapter3;
 
 import java.util.ArrayList;
 import java.util.EmptyStackException;
+import java.util.Stack;
 
-
-public class SetOfStacks<T> {
-	public static class SetNode<T> {
-		private T data;
-		private SetNode<T> next;
-		public SetNode(T data) {
-			this.data = data;
-		}
-		public T getData(){
-			return this.data;
-		}
-	}
-	
-	public ArrayList<SetNode<T>> setOfTop;
-	public int sizeOfStacks;
-	public int currentSize;
-	
-	public SetOfStacks(int sizeOfStacks){
-		setOfTop = new ArrayList<SetNode<T>>();
-		this.sizeOfStacks = sizeOfStacks;
-		this.currentSize = 0;
-	}
-	
-	public int currentNumStack() {
-		return (currentSize-1)/sizeOfStacks + 1;
-	}
-	
-	public int reallNumStack() {
-		return setOfTop.size();
-	}
-	
-	public void push(T item) { 
-		currentSize ++;
-		SetNode<T> node = new SetNode<T>(item);
-		if (currentNumStack() > reallNumStack()) {
-			setOfTop.add(node);
-			if (currentNumStack()>1) {
-				node.next = setOfTop.get(currentNumStack()-2);
-			}
-		} else {
-			SetNode<T> top = setOfTop.get(currentNumStack()-1);
-			node.next = top;
-			setOfTop.set(currentNumStack()-1, node);
-		}
-		//System.out.println("currentSize "+currentSize);
-		//System.out.println("Current Num of Stack"+(currentNumStack()-1)+" Top is "+setOfTop.get(currentNumStack()-1).getData());
-	}
-	
-	public T pop (){
-		SetNode<T> top = setOfTop.get(currentNumStack()-1);
-		//System.out.println("top "+top.getData());
-		//System.out.println("size top "+setOfTop.size());
-		if (top == null) throw new EmptyStackException();
-		currentSize --;
-		if (currentNumStack() < reallNumStack()) {
-			T data = top.getData();
-			setOfTop.remove(top);
-			return data;
-		} else {
-			T data = top.getData();
-			setOfTop.set(currentNumStack()-1, top.next);
-			return data;
-		}
-	}
-	
-	public T peek(){
-		SetNode<T> top = setOfTop.get(currentNumStack()-1);
-		T data = top.getData();
-		return data;
+public class SetOfStacks {
+	ArrayList<Stack> stacks = new ArrayList<Stack> ();
+	private static int CAPACITY = 3;
+		
+	public Stack getLastStack() {
+		if (stacks.size() == 0)
+			return null;
+		return stacks.get(stacks.size()-1);
 	}
 	
 	public boolean isEmpty() {
-		if (reallNumStack() == 0) {
-			return true;
+		return (stacks.size() == 0);
+	}
+	
+	public boolean isFull() {
+		return (getLastStack().size() == CAPACITY);
+	}
+	
+	public void push(int item) {
+		if ( getLastStack()== null || isFull()) {
+			Stack stack = new Stack();
+			stacks.add(stack);
+			stack.push(item);
 		} else {
-			return false;
+			getLastStack().push(item);
 		}
 	}
 	
-	public static void main(String args[]) {
-		SetOfStacks<Integer> setOfStacks = new SetOfStacks<Integer>(3);
-		setOfStacks.push(1);
-		setOfStacks.push(2);
-		setOfStacks.push(3);
-		setOfStacks.push(4);
-		setOfStacks.push(5);
-		setOfStacks.push(6);
-		setOfStacks.push(7);
-		System.out.println(setOfStacks.pop());
-		System.out.println(setOfStacks.pop());
-		System.out.println(setOfStacks.pop());
-		System.out.println(setOfStacks.pop());
-		System.out.println(setOfStacks.pop());
-		System.out.println(setOfStacks.pop());
-		System.out.println(setOfStacks.pop());
+	public int pop() {
+		Stack stack = getLastStack();
+		if (stack == null) throw new EmptyStackException();
+		int item = (int) stack.pop();
+		if (stack.size() == 0) stacks.remove(stacks.size()-1);
+		return item;
 	}
+	
+	public static void main(String[] args) {
+		SetOfStacks stacks = new SetOfStacks();
+		for (int i = 0; i < 7; i++) {
+			stacks.push(i+1);
+		}
+		for (int i = 0; i <7; i++) {
+			System.out.println("pop "+stacks.pop());
+		}
+	}	
 }
