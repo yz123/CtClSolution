2723540e9128f98ec1c1418d76e7839060e1b623
Jinhai ZHOU <zhoujinhaixd@gmail.com>
2016-06-04 23:40:44 +0200

    another solution

--- src/Chapter4/ValidateBST.java
+++ src/Chapter4/ValidateBST.java
@@ -1,39 +1,46 @@
 package Chapter4;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import CtCILibrary.AssortedMethods;
 import CtCILibrary.TreeNode;
 
 public class ValidateBST {
-	public static int maxBST(TreeNode root) {
-		if (root == null)
-			return 0;
-		int middle = root.data;
-		int left = 0;
-		int right = 0;
-		if (root.left != null) {
-			left = root.left.data;
-			if (left > middle)
-				return Integer.MAX_VALUE;
+
+	public static void inOrder(TreeNode root, Queue<Integer> q) {
+		if (root !=null) {
+			inOrder(root.left, q);
+			q.add(root.data);
+			inOrder(root.right, q);
 		}
-		if (root.right != null) {
-			right = root.right.data;
-			if (right < middle)
-				return Integer.MAX_VALUE;
-		}
-		return Math.max(maxBST(root.left), maxBST(root.right));
 	}
+	
 	public static boolean isBST(TreeNode root) {
-		if (maxBST(root) == Integer.MAX_VALUE)
-			return false;
-		else
+		Queue<Integer> q = new LinkedList<Integer>();
+		inOrder(root, q);
+		int data = 0;
+		if (!q.isEmpty())
+			data = q.poll();
+		else 
+		{
 			return true;
+		}
+		while(!q.isEmpty()) {
+			if (data > q.peek())
+				return false;
+			data = q.poll();
+		}
+		System.out.println("empty");
+		return true;
 	}
+	
 	public static void main(String[] args) {
 		// Create balanced tree
 		int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		TreeNode root = TreeNode.createMinimalBST(array);
 		System.out.println("Root? " + root.data);
-		System.out.println("Is balanced? " + isBST(root));
+		System.out.println("Is BST? " + isBST(root));
 		
 		// Could be balanced, actually, but it's very unlikely...
 		TreeNode unbalanced = new TreeNode(10);
@@ -41,6 +48,6 @@ public class ValidateBST {
 			unbalanced.insertInOrder(AssortedMethods.randomIntInRange(0, 100));
 		}
 		System.out.println("Root? " + unbalanced.data);
-		System.out.println("Is balanced? " + isBST(unbalanced));
+		System.out.println("Is BST? " + isBST(unbalanced));
 	}
 }
