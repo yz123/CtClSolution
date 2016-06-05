9eff94480d6c6353dd3a4dbfb6be81fd34e374a7
Jinhai ZHOU <zhoujinhaixd@gmail.com>
2016-06-05 22:44:17 +0200

    add test code to ValidateBST

--- src/Chapter4/ValidateBST.java
+++ src/Chapter4/ValidateBST.java
@@ -31,23 +31,61 @@ public class ValidateBST {
 				return false;
 			data = q.poll();
 		}
-		System.out.println("empty");
+		//System.out.println("empty");
 		return true;
 	}
 	
 	public static void main(String[] args) {
-		// Create balanced tree
-		int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		TreeNode root = TreeNode.createMinimalBST(array);
-		System.out.println("Root? " + root.data);
-		System.out.println("Is BST? " + isBST(root));
-		
-		// Could be balanced, actually, but it's very unlikely...
-		TreeNode unbalanced = new TreeNode(10);
-		for (int i = 0; i < 10; i++) {
-			unbalanced.insertInOrder(AssortedMethods.randomIntInRange(0, 100));
-		}
-		System.out.println("Root? " + unbalanced.data);
-		System.out.println("Is BST? " + isBST(unbalanced));
+		int[] array = {Integer.MIN_VALUE, Integer.MAX_VALUE - 2, Integer.MAX_VALUE - 1, Integer.MAX_VALUE};
+		TreeNode node = TreeNode.createMinimalBST(array);
+		//node.left.data = 5;
+		//node.left.right.data = 3;
+		System.out.println(isBST(node));
+
+		test();
+	}
+
+	public static void test() {
+		TreeNode node;
+		boolean condition;
+		System.out.println("test cases for equals condition.");
+
+		/* Expect true: for left child: node.data <= last_printed.
+   2
+  / \
+ /   \
+ 2   3
+      \
+      4
+		*/
+		int[] array2 = {1, 2, 3, 4};
+		node = TreeNode.createMinimalBST(array2);
+		node.left.data = 2;
+		node.print();
+		//last_printed = null;
+		condition = isBST(node);
+		System.out.println("should be true: " + condition);
+
+		/* Expect false: for right child: node.data <= last_printed.
+   2
+  / \
+ /   \
+ 1   2
+      \
+      4
+		 */
+		int[] array3 = {1, 2, 3, 4};
+		node = TreeNode.createMinimalBST(array3);
+		node.right.data = 2;
+		node.print();
+		//last_printed = null;
+		condition = isBST(node);
+		System.out.println("should be false: " + condition);
 	}
 }
+
+/*
+ * performance: time complexity O(n), space complexity O(n)
+ * weakness: need extra space, cannot handle duplicate values
+ * to do: build off property that LEFT <= CURRENT < RIGHT 
+ */
